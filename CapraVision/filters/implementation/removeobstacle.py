9bc68c7e9e1f6ad799f23782015ff6a9a814bdea
benoit <benoxoft@gmail.com>
2012-10-11 09:20:55 -0400

    Better code execution window added filter to remove obstacles

--- CapraVision/filters/implementation/removeobstacle.py
+++ CapraVision/filters/implementation/removeobstacle.py
@@ -4,17 +4,25 @@ import cv2
 class RemoveObstacle:
     
     def __init__(self):
-        pass
+        self.threshold = 50000
+        self._kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3), (0,0))
     
     def execute(self, image):
-        image = cv2.blur(image, (3,3))
         blue, green, red = cv2.split(image.astype('uint16'))
         new_red = red**2
-        threshold = (new_red < 62000)
-        blue[threshold] = 0
-        green[threshold] = 0
+        threshold = (new_red < self.threshold)
         red[threshold] = 0
-        image[:,:,0] = blue
-        image[:,:,1] = green 
-        image[:,:,2] = red 
+        red = cv2.morphologyEx(
+                  red, cv2.MORPH_CLOSE, self._kernel, iterations=10)
+        mask = red > 0
+        image[mask] = 0
+        
+        #blue[threshold] = 0
+        #green[threshold] = 0
+        #red[threshold] = 0
+        #image[:,:,0] = blue
+        #image[:,:,1] = green 
+        #image[:,:,2] = red 
         return image
+        
+        
