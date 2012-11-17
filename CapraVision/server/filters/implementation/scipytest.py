d4e48ab56d677e6e0c50799522856c653cde4042
benoxoft <benoxoft@gmail.com>
2012-11-17 16:16:12 -0500

    Example about how to use scipy.weave in a filter.

---
+++ CapraVision/server/filters/implementation/scipytest.py
@@ -0,0 +1,32 @@
+
+from scipy import weave
+
+class ScipyExample:
+    """Example on how to use scipy.weave inside filters.
+        The code loop inside the entire image 
+        and reduce the value of each pixels by half"""
+    
+    def __init__(self):
+        pass
+
+    def execute(self, image):
+        code = """
+            int rows = Nimage[0];
+            int cols = Nimage[1];
+            int depth = Nimage[2];   
+            for (int i=0; i < rows; i++)
+            {
+                for (int j=0; j < cols; j++)
+                {
+                    for (int k=0; k< depth; ++k)
+                    {
+                        image[(i*cols + j)*depth + k] /= 2;
+                    }
+                }
+            }
+            """
+        
+        weave.inline(code, ['image'])
+        return image
+    
+            
