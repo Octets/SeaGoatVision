564793f6f7cd03905b7834f902e1e53d7a524f81
Mathieu Benoit <mathben963@gmail.com>
2013-02-03 16:57:34 -0500

    Can now detect eyes. - commit 0b45445060df542aa3

--- CapraVision/server/filters/implementation/facedetect.py
+++ CapraVision/server/filters/implementation/facedetect.py
@@ -1,10 +1,13 @@
 
+# http://docs.opencv.org/doc/tutorials/objdetect/cascade_classifier/cascade_classifier.html
+
 import cv2
 import cv2.cv as cv
 
 import os
 
 class FaceDetection:
+    """Detect faces and eyes"""
     
     def __init__(self):
         self.eye_detect_name = os.path.join('data', 
@@ -27,9 +30,39 @@ class FaceDetection:
                                                    0|cv.CV_HAAR_SCALE_IMAGE, 
                                                    (30, 30)
                                                 )
-        for x, y, width, height in faces:
-            cv2.rectangle(image, (x, y), (x+width, y+height), 
-                          (0, 0, 255))
-    
+        for face in faces:
+            faceimg = self.draw_rectangle(image, face, (0, 0, 255))
+            
+            eyes = self.eye_cascade.detectMultiScale(faceimg,
+                                                   1.1, 
+                                                   2, 
+                                                   0|cv.CV_HAAR_SCALE_IMAGE, 
+                                                   (30, 30)
+                                                )
+            for eye in eyes:
+                self.draw_rectangle(image, 
+                        (face[0] + eye[0], face[1] + eye[1], eye[2], eye[3]), 
+                        (255, 0, 0))
+            
         return image
+
+    def draw_rectangle(self, image, coord, color):
+        x, y, w, h = coord
+        miny = y
+        if miny < 0:
+            miny = 0
+        maxy = y + h
+        if maxy > image.shape[0]:
+            maxy = image.shape[0]
+        minx = x 
+        if minx < 0:
+            minx = 0
+        maxx = x + w
+        if maxx > image.shape[1]:
+            maxx = image.shape[1]
+
+        cv2.rectangle(image, (minx, miny), (maxx, maxy), 
+                      color, 3)
+    
+        return image[miny:maxy, minx:maxx]
     
