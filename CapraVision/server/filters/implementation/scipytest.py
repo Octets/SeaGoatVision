24fffc9811786491892ad61489b9e63ac6e20b9e
Mathieu Benoit <mathben963@gmail.com>
2013-03-01 18:53:56 -0500

    Add notification example with scipytest - filter in c.

--- CapraVision/server/filters/implementation/scipytest.py
+++ CapraVision/server/filters/implementation/scipytest.py
@@ -6,21 +6,27 @@ from CapraVision.server.filters import dataextract
 
 class ScipyExample(dataextract.DataExtractor):
     """Example on how to use scipy.weave inside filters.
-        The code loop inside the entire image 
+        The code loop inside the entire image
         and reduce the value of each pixels by half"""
-    
+
     def __init__(self):
         dataextract.DataExtractor.__init__(self)
 
     def execute(self, image):
+        #self.notify_output_observers("ScipyTestPy: j=6 \n")
+        notify = self.notify_output_observers
+
         img1 = cv.fromarray(image)
         weave.inline(
         """
+        py::tuple notify_args(1);
+        notify_args[0] = "patatoum";
+        notify.call(notify_args);
         cv::Mat mat(get_cvmat(img1));
         //printf("addr %d %d\\n",  mat.rows, mat.cols);
-        cv::circle(mat, cv::Point(mat.rows/2, mat.cols/2), mat.cols/4, cv::Scalar(255, 0, 0), -1);
+        cv::circle(mat, cv::Point(mat.rows/2, mat.cols/2), mat.cols/4, cv::Scalar(255, 0, 255), -1);
         """,
-        arg_names = ['img1'],
+        arg_names = ['img1', 'notify'],
         include_dirs = ['/usr/local/include/opencv/'],
         headers = ['<cv.h>', '<cxcore.h>'],
         #libraries = ['ml', 'cvaux', 'highgui', 'cv', 'cxcore'],
@@ -36,6 +42,5 @@ class ScipyExample(dataextract.DataExtractor):
         """
         )
         img1 = numpy.asarray(img1)
-        self.notify_output_observers("ScipyExample: x=20 y=50\n")
         return img1
 
