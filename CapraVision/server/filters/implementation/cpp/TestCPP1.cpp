02b32ef7a2474c790fe9eab94422df8312740073
benoit <benoxoft@gmail.com>
2013-03-07 21:56:34 -0500

    Fixed help in C++ filters

--- CapraVision/server/filters/implementation/cpp/TestCPP1.cpp
+++ CapraVision/server/filters/implementation/cpp/TestCPP1.cpp
@@ -1,11 +1,11 @@
 #include "opencv2/opencv.hpp"
 //#include "opencv2/gpu/gpu.hpp"
 
-#define DOCSTRING = "C++ Example";
+#define DOCSTRING "C++ Example Test #1"
 
 cv::Mat execute(cv::Mat image)
 {
-    cv::cvtColor(image, image, CV_BGR2YUV);
+    cv::cvtColor(image, image, CV_BGR2HSV);
     //std::cout << image << std::endl;
     return image;
 }
