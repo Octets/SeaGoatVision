898ee7c43fbf8661da47ffa2aec3830d59cff86a
benoit <benoxoft@gmail.com>
2013-03-07 21:18:59 -0500

    Fix multiple C++ files

--- CapraVision/server/filters/implementation/cpp/__init__.py
+++ CapraVision/server/filters/implementation/cpp/__init__.py
@@ -3,30 +3,28 @@ import sys
 import numpy as np
 import scipy.weave.ext_tools as ext_tools
 
-cppmodules = []
 image = np.zeros((1,1), dtype=np.uint8)
 help = ""
 
-mod = ext_tools.ext_module('cppfilters')
-mod.customize.add_header('<opencv2/opencv.hpp>')
-mod.customize.add_header('<opencv2/gpu/gpu.hpp>')
-
-mod.customize.add_extra_link_arg("`pkg-config --cflags --libs opencv`")
+extcode = """
+    cv::Mat mat(Nimage[0], Nimage[1], CV_8UC(3), image);
+    cv::Mat ret = execute(mat);
+    if (mat.data != ret.data)
+        ret.copyTo(mat);
+    """
 
 dirname = os.path.dirname(__file__)
 for f in os.listdir(dirname):
     if not f.endswith(".cpp"):
         continue
     filename, _ = os.path.splitext(f)
-    cppmodules.append(filename)
     cppcode = open(os.path.join(dirname, f)).read()
 
-    extcode = """
-        cv::Mat mat(Nimage[0], Nimage[1], CV_8UC(3), image);
-        cv::Mat ret = execute(mat);
-        if (mat.data != ret.data)
-            ret.copyTo(mat);
-        """
+    mod = ext_tools.ext_module(filename)
+    [mod.customize.add_header(line.replace('#include ', '')) 
+                             for line in cppcode.split('\n') 
+                             if line.startswith('#include')]
+    mod.customize.add_extra_link_arg("`pkg-config --cflags --libs opencv`")
 
     func = ext_tools.ext_function(filename, extcode,['image'])
     func.customize.add_support_code(cppcode)
@@ -41,23 +39,21 @@ for f in os.listdir(dirname):
     #helpmod.customize.add_support_code(cppcode)
     #mod.add_function(helpmod)
 
-try:
-    mod.compile()
-    def create_execute(cppfunc):
-        def execute(self, image):
-            cppfunc(image)
-            return image
-        return execute
+    try:
+        mod.compile()
+        def create_execute(cppfunc):
+            def execute(self, image):
+                cppfunc(image)
+                return image
+            return execute
 
-    import cppfilters
-    from cppfilters import *
-    for module in cppmodules:
+        cppmodule = __import__(filename)
         #code = """from cppfilters import %(module)s""" % {'module' : module}
         #exec code
-        clazz = type(module, (object,),
-                     {'execute' : create_execute(globals()[filename]),
+        clazz = type(filename, (object,),
+                     {'execute' : create_execute(getattr(cppmodule, filename)),
                       '__doc__' : "C++ filter"})#getattr(cppfilters, 'help_' + filename)()})
-        setattr(sys.modules[__name__], module, clazz)
+        setattr(sys.modules[__name__], filename, clazz)
         del clazz
-except Exception as e:
-    print(e)
+    except Exception as e:
+        sys.stderr.write(e)
