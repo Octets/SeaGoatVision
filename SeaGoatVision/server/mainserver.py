5742b2f56625ee5263838cccea1b2cb708d8021e
Mathieu Benoit <mathben963@gmail.com>
2013-03-12 01:44:51 -0400

    Fix can set host for server tcp

--- SeaGoatVision/server/mainserver.py
+++ SeaGoatVision/server/mainserver.py
@@ -96,7 +96,6 @@ def check_pid(pid):
 
 if __name__ == '__main__':
     # Project path is parent directory
-    import os
     parentdir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     os.sys.path.insert(0, parentdir)
     run()
