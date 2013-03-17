459c0f7877513b9754bb3b8ec3f31cad3a8e2df7
Mathieu Benoit <mathben963@gmail.com>
2013-03-17 17:42:51 -0400

    Adapt cpp standalone to Filter class.

--- SeaGoatVision/server/filters/implementation/__init__.py
+++ SeaGoatVision/server/filters/implementation/__init__.py
@@ -35,7 +35,7 @@ import traceback
 import numpy as np
 import scipy.weave.ext_tools as ext_tools
 
-from SeaGoatVision.server.filters.dataextract import DataExtract
+from SeaGoatVision.server.core.filter import Filter
 import SeaGoatVision.globals as g
 
 ##
@@ -154,7 +154,7 @@ def compile_cpp_filters():
 
             cppmodule = __import__(modname)
 
-            clazz = type(filename, (DataExtract,),
+            clazz = type(filename, (Filter,),
                          {'__init__' : init(),
                           'execute' : create_execute(getattr(cppmodule, filename)),
                           '__doc__' : getattr(cppmodule, 'help_' + filename)()})
