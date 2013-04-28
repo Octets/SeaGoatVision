0e58c68d257432ed6da5ced60dd5303c9386f8bd
benoit <benoxoft@gmail.com>
2013-04-28 02:23:51 -0400

    C++ parameters almost working.

--- CapraVision/server/filters/implementation/TestCPP1.cpp
+++ CapraVision/server/filters/implementation/TestCPP1.cpp
@@ -3,9 +3,26 @@
 
 #define DOCSTRING "C++ Example Test #1"
 
-cv::Mat execute(cv::Mat image)
-{
-    cv::cvtColor(image, image, CV_BGR2HSV);
-    //std::cout << image << std::endl;
-    return image;
-}
+class TestCPP1 : public Filter {
+
+	int x;
+
+public:
+
+	void init() {
+		x = ParameterAsInt("x", 0, 255, 30);
+	}
+
+	void configure() {
+		x = ParameterAsInt("x", 0, 255, 30);
+	}
+
+	cv::Mat execute(cv::Mat image)
+	{
+
+		cv::cvtColor(image, image, CV_BGR2HSV);
+		std::cout << x << std::endl;
+		return image;
+	}
+
+};
