2b5c1a0f4a2132da25f06fc632f4ffb482b611be
Mathieu Benoit <mathben963@gmail.com>
2013-06-18 16:50:27 -0400

    Qt: be sure to clean the widget if click on filterchain.

--- SeaGoatVision/client/qt/main/WinFilter.py
+++ SeaGoatVision/client/qt/main/WinFilter.py
@@ -68,17 +68,18 @@ class WinFilter(QtGui.QDockWidget):
             self.cb_param.addItem(name)
 
     def set_filter(self):
-        self.execution_name = self.shared_info.get("execution")
-        self.filter_name = self.shared_info.get("filter")
-        if not self.filter_name:
-            return
-
         self.ui.txt_search.setText("")
-
         self.dct_param = {}
         self.cb_param.currentIndexChanged.disconnect(self.on_cb_param_item_changed)
         self.cb_param.clear()
         self.cb_param.currentIndexChanged.connect(self.on_cb_param_item_changed)
+
+        self.execution_name = self.shared_info.get("execution")
+        self.filter_name = self.shared_info.get("filter")
+        if not self.filter_name:
+            self.clear_widget()
+            self.ui.lbl_param_name.setText("Empty params")
+            return
 
         self.lst_param = self.controller.get_params_filterchain(self.execution_name, filter_name=self.filter_name)
         if self.lst_param is None:
@@ -99,15 +100,18 @@ class WinFilter(QtGui.QDockWidget):
     def on_cb_param_item_changed(self, index):
         self.ui.lbl_param_name.setText("%s - %s" % (self.execution_name, self.filter_name))
 
-        item = self.layout.takeAt(0)
-        if item:
-            item.widget().deleteLater()
+        self.clear_widget()
 
         if index == -1:
             return
 
         param = self.lst_param[index]
         self.layout.addWidget(self.getWidget(param))
+
+    def clear_widget(self):
+        item = self.layout.takeAt(0)
+        if item:
+            item.widget().deleteLater()
 
     def getWidget(self, param):
         groupBox = QtGui.QGroupBox()
