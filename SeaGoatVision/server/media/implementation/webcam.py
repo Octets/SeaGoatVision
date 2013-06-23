db748f2d045047567faf7ec9bda17b1b793e8659
Mathieu Benoit <mathben963@gmail.com>
2013-06-23 01:33:15 -0400

    Webcam: add property FPS

--- SeaGoatVision/server/media/implementation/webcam.py
+++ SeaGoatVision/server/media/implementation/webcam.py
@@ -43,25 +43,37 @@ class Webcam(Media_streaming):
                                "640x480":(640, 480),
                                "1024x768":(1024, 768),
                                "1280x960":(1280, 960)}
-        self.shape = self.dct_resolution[self.actual_resolution_name]
+        self.actual_fps_name = "30"
+        self.dct_fps = {self.actual_fps_name:30, "15":15, "7.5":7.5}
 
         self.deserialize(self.config.read_media(self.get_name()))
 
     def serialize(self):
-        return {"resolution":self.actual_resolution_name}
+        return {"resolution":self.actual_resolution_name, "fps":self.actual_fps_name}
 
     def deserialize(self, data):
+        if not data:
+            return False
         if type(data) is not dict:
             print("Error: Wrong format data, suppose to be dict into camera %s" % self.get_name())
+            return False
         res = data.get("resolution", None)
         if res:
             self.change_resolution(res)
+        res = data.get("fps", None)
+        if res:
+            self.change_fps(res)
+        return TRue
 
     def open(self):
         try:
+            shape = self.dct_resolution[self.actual_resolution_name]
+            fps = self.dct_fps[self.actual_fps_name]
+
             self.video = cv2.VideoCapture(self.own_config.no)
-            self.video.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, self.shape[0])
-            self.video.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, self.shape[1])
+            self.video.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, shape[0])
+            self.video.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, shape[1])
+            self.video.set(cv2.cv.CV_CAP_PROP_FPS, fps)
         except Exception as e:
             print("Error: %s" % e)
             return False
@@ -86,14 +98,25 @@ class Webcam(Media_streaming):
                   (resolution, self.dct_resolution.keys(), self.get_name()))
             return False
         self.actual_resolution_name = resolution
-        self.shape = self.dct_resolution[resolution]
+        return self.reload()
+
+    def change_fps(self, fps):
+        """ Param: fps type string, need to be a key of dct_fps"""
+        if fps not in self.dct_fps:
+            print("Error: The key %s not in the list of fps %s of media %s." %
+                  (resolution, self.dct_fps.keys(), self.get_name()))
+            return False
+        self.actual_fps_name = fps
         return self.reload()
 
     def get_properties_param(self):
         resolution = Param("resolution", self.actual_resolution_name, lst_value=self.dct_resolution.keys())
-        return [resolution]
+        fps = Param("fps", self.actual_fps_name, lst_value=self.dct_fps.keys())
+        return [resolution, fps]
 
     def update_property_param(self, param_name, value):
         if param_name == "resolution":
             return self.change_resolution(value)
+        elif param_name == "fps":
+            return self.change_fps(value)
         return False
