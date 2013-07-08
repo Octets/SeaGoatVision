629f3d9487c43e4c87ef9ed8c72b90b40338b876
Mathieu Benoit <mathben963@gmail.com>
2013-07-07 22:36:19 -0400

    Cpp_code: Filter: Improve error when missing key param. Clean import into WinViewer.

--- SeaGoatVision/client/qt/main/WinViewer.py
+++ SeaGoatVision/client/qt/main/WinViewer.py
@@ -17,8 +17,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# from gi.repository import GObject
-
 """
 Description : Windows to "view" a filterchain execution
 """
